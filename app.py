5bf2bcc7020963de468e11a83426105be59a2898
Aaron Ho <aaron.ho@fliway.com>
2025-04-08 08:59:22 +1200

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for, make_response, send_file
+from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from flask_wtf.file import FileAllowed, FileRequired
+from excel_formatter import standard_order, inventory_inbound
+import datetime
+
+app = Flask(__name__)
+
+# Configuration
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+app.config['DATA_FOLDER'] = 'data'
+app.config['RAW_FOLDER'] = 'data/raw'
+app.config['PROCESSED_FOLDER'] = 'data/processed'
+app.config['WTF_CSRF_ENABLED'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Ensure upload folder exists
+if not os.path.exists(app.config['DATA_FOLDER']):
+    os.makedirs(app.config['DATA_FOLDER'])
+    os.makedirs(app.config['RAW_FOLDER'])
+    os.makedirs(app.config['PROCESSED_FOLDER'])
+
+class UploadForm(FlaskForm):
+    file = FileField('Excel File', validators=[
+        FileRequired(),
+        FileAllowed(['xlsx', 'xls'], 'Excel files only!')
+    ])
+    submit = SubmitField('Upload')
+
+@app.errorhandler(413)
+
+def too_large(e):
+    return make_response("File is too large", 413)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in {'xlsx', 'xls'}
+
+@app.route('/')
+def index():
+    return redirect(url_for('upload'))
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # file specific processing
+            if filename == 'Standard_Order_Details_Created_for_account_99930_NZ.xlsx':
+                flash('File successfully uploaded')
+                raw_filepath = f'data/raw/{filename}'
+                file.save(raw_filepath)
+    
+                processed_file = standard_order(raw_filepath)
+                print(f'datetime: {datetime.datetime.now()}')
+                return send_file(processed_file, as_attachment=True)
+
+            elif filename == 'Inventory_Inbound_with_KPI_Details_NZ.xlsx':
+                flash('File successfully uploaded')
+                raw_filepath = f'data/raw/{filename}'
+                file.save(raw_filepath)
+                
+                
+                processed_file = inventory_inbound(raw_filepath)
+                print(f'datetime: {datetime.datetime.now()}')
+                return send_file(processed_file, as_attachment=True)
+
+            else: 
+                flash('Unsupported file')
+
+            return redirect(url_for('upload'))
+        else:
+            flash('Invalid file type')
+    return render_template('upload.html', form=form)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ excel_formatter.py
@@ -0,0 +1,190 @@
+import csv
+import openpyxl as o
+import os
+import pandas as pd
+import shutil
+import subprocess
+import sqlite3
+import time
+
+def win_convert_csv_skip_rows_com(in_file, rows, out_file):
+    """use com to convert xlsx to csv (slowest)"""
+    workbook = None
+    excel = None
+    try:
+        import win32com.client as win32
+        excel = win32.Dispatch('Excel.Application')  # Late binding
+        excel.Visible = False
+        
+        # Open and convert the file
+        workbook = excel.Workbooks.Open(in_file)
+        sheet = workbook.ActiveSheet
+        
+        # Delete first 7 rows
+        sheet.Rows(f"1:{rows}").Delete()
+        
+        # Save as CSV
+        workbook.SaveAs(out_file, FileFormat=6)  # 6 is CSV format
+    except Exception as e:
+        print(f"Error converting file: {e}")
+    finally:
+        
+        # Clean up
+        if workbook:
+            workbook.Close(SaveChanges=False)
+        if excel:
+            excel.Quit()
+
+def win_convert_csv_skip_rows_soffice(in_file, rows, out_file):
+        """use soffice to convert xlsx to csv (fastest)"""
+        subprocess.run([r"C:\Program Files\LibreOffice\program\soffice.exe", "--headless", "--convert-to", "csv", in_file])
+        csv_file = os.path.basename(in_file.replace(".xlsx", ".csv"))
+        
+        # read file
+        with open(csv_file, 'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)  # store all rows in a list
+
+        # skip the specified number of rows 
+        data = data[rows:]
+
+        # write the modified data back to file
+        with open(out_file, 'w', newline='') as f_out:
+            writer = csv.writer(f_out)
+            writer.writerows(data)  # Write all remaining rows
+
+def standard_order(file_path):
+    """Formats standard order file and returns the processed file path."""
+    start_time = time.time()  # Start timing
+    
+    # appending appropriate suffixes for transition files
+    excel_transition_file = 'transition.xlsx'
+    output_file = f'data/processed/{os.path.basename(file_path.replace(".xlsx", "_cleaned.xlsx"))}'
+
+    # Convert xlsx to csv using win32
+    file_csv = os.path.basename(file_path.replace(".xlsx", ".csv"))
+    file_csv = os.path.abspath(file_csv)
+    
+    # conditional csv conversion based on operating system
+    win_convert_csv_skip_rows_soffice(file_path, 7, file_csv)
+
+    # load csv into pandas
+    df = pd.read_csv(file_csv, encoding='ISO-8859-1')
+    df = df.astype(str)  # convert all data to string to enable SQL filtering
+
+    # sql query to filter relevant columns 
+    query = 'SELECT "Service O", "Order Pla", "Order Shi" FROM my_table'
+
+    # filter using sqlite
+    conn = sqlite3.connect(":memory:")  # use an in-memory database
+    df.to_sql("my_table", conn, if_exists="replace", index=False)
+    filtered_df = pd.read_sql(query, conn)
+
+    # save filtered data to an excel file
+    filtered_df.to_excel(excel_transition_file, index=False)
+
+    # open the workbook for formatting
+    wb = o.load_workbook(excel_transition_file)
+    ws = wb.active
+
+    # increase font size
+    data_font = o.styles.Font(size=20)
+    for row in ws:
+        for cell in row:
+            cell.font = data_font
+
+    # increase column widths
+    for col in ws.columns:
+        max_length = 35
+        col_letter = col[0].column_letter
+        for cell in col:
+            try:
+                max_length = max(max_length, len(str(cell.value)))
+            except:
+                pass
+        ws.column_dimensions[col_letter].width = max_length + 2
+
+    wb.save(output_file)
+
+    # clean up temporary files
+    os.remove(file_csv)
+    os.remove(excel_transition_file)
+
+    # Calculate execution time
+    execution_time = time.time() - start_time
+    print(f"Standard order processing took {execution_time:.2f} seconds")
+
+    # returns file path
+    return output_file
+
+def inventory_inbound(file_path):
+    """Formats standard order file and returns the processed file path."""
+    start_time = time.time()  # Start timing
+
+    # appending appropriate suffixes for transition files
+    excel_transition_file = 'transition.xlsx'
+    output_file = f'data/processed/{os.path.basename(file_path.replace(".xlsx", "_cleaned.xlsx"))}'
+    # Convert xlsx to csv using win32
+    file_csv = os.path.basename(file_path.replace(".xlsx", ".csv"))
+    file_csv = os.path.abspath(file_csv)
+
+    # conditional csv conversion based on operating system
+    win_convert_csv_skip_rows_soffice(file_path, 4, file_csv)
+    
+    # load csv into pandas
+    df = pd.read_csv(file_csv, encoding='ISO-8859-1')
+    df = df.dropna(how='all') # drop rows with no values
+    df = df.astype(str)  # convert all data to string to enable SQL filtering
+
+    # sql query to filter relevant columns 
+    query = 'SELECT "Trans Id", "Dock Date/Time Stamp", "Inbound Date" FROM my_table'
+
+    # filter using sqlite
+    conn = sqlite3.connect(":memory:")  # use an in-memory database
+    df.to_sql("my_table", conn, if_exists="replace", index=False)
+    filtered_df = pd.read_sql(query, conn)
+
+    # save filtered data to an excel file
+    filtered_df.to_excel(excel_transition_file, index=False)
+
+    # open the workbook for formatting
+    wb = o.load_workbook(excel_transition_file)
+    ws = wb.active
+
+    # increase font size
+    data_font = o.styles.Font(size=20)
+    for row in ws:
+        for cell in row:
+            cell.font = data_font
+
+    # increase column widths
+    for col in ws.columns:
+        max_length = 35
+        col_letter = col[0].column_letter
+        for cell in col:
+            try:
+                max_length = max(max_length, len(str(cell.value)))
+            except:
+                pass
+        ws.column_dimensions[col_letter].width = max_length + 2
+
+    # change first column to numberic dtype
+    col_letter = o.utils.get_column_letter(1)
+    ws.column_dimensions[col_letter].number_format = 'General'
+    for cell in ws[col_letter][1:]:
+        if cell.value is not None:  # Skip header or empty cells
+            cell.value = int(float(cell.value))
+
+    # save the final cleaned file
+    wb.save(output_file)
+
+    # clean up temporary files
+    os.remove(file_csv)
+    os.remove(excel_transition_file)
+
+    # Calculate execution time
+    execution_time = time.time() - start_time
+    print(f"Inventory inbound processing took {execution_time:.2f} seconds")
+
+    # returns file path
+    return output_file
